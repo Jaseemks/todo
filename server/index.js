144fa8d9e7f5ccaa924df40e5c25781edb55d74a
jaseem <jaseem6396jaz@gmail.com>
2024-11-21 01:01:56 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -7,7 +7,8 @@ const app = express();
 
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "todo-list-webapp-alpha.vercel.app",
+    // origin: "http://localhost:5173",
     credentials: true,
 }));
 
